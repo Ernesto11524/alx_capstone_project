d97779426917243ceac1e03c93971d30d233a0c8
Ernest Mpiani <ernestmpiani15@gmail.com>
2025-10-06 18:59:12 +0000

    Added two form classes which inherits from the built in form class and they are responsible for registration of new books and also borrowing of a book

--- library_management_system/book/forms.py
+++ library_management_system/book/forms.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django import forms
+from .models import Book, Transaction
 
 
 # This is a class designed to include other relevant fields like firstname, lastname
@@ -8,3 +10,20 @@ class ModifiedUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+
+
+# This is a form class which handles the registration of books in the database.
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+
+# This is a form class which handles the book transactions, i.e. borrowed books in the system.
+class TransactionForm(forms.ModelForm):
+    time_of_return = forms.DateTimeField(required=False)
+    returned = forms.BooleanField(required=False)
+
+    class Meta:
+        model = Transaction
+        fields = ['user', 'book', 'borrow_duration', 'check_out', 'time_of_return', 'returned']
