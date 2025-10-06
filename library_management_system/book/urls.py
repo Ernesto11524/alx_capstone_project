02475951873d2244d50ec7ff00b048a75fcc8e56
Ernest Mpiani <ernestmpiani15@gmail.com>
2025-10-06 19:01:37 +0000

    Added extra routes for the new view functions added to the admin dashboard

--- library_management_system/book/urls.py
+++ library_management_system/book/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views as book_views
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', book_views.register, name='register'),
@@ -8,4 +7,7 @@ urlpatterns = [
     path('logout/', book_views.custom_logout, name='logout'),
     path('admin_home/', book_views.admin_home, name='admin_home'),
     path('user_home/', book_views.user_home, name='user_home'),
+    path('manage_books/', book_views.manage_books, name='manage_books'),
+    path('manage_users/', book_views.manage_users, name='manage_users'),
+    path('borrow_records/', book_views.borrow_records, name='borrow_records'),
 ]
