17f8292677c8778f205431a203aea686469d1cf5
Ernest Mpiani <ernestmpiani15@gmail.com>
2025-10-08 14:41:45 +0000

    Added filtering settings

--- library_management_system/library_management_system/settings.py
+++ library_management_system/library_management_system/settings.py
@@ -127,6 +127,9 @@ LOGIN_REDIRECT_URL = 'user_home'
 LOGOUT_REDIRECT_URL = 'login'
 
 REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5
+    'PAGE_SIZE': 5,
 }
