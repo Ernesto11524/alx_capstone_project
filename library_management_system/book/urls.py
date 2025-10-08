beda5680551ae404777c89de20ebd0baafef08f8
Ernest Mpiani <ernestmpiani15@gmail.com>
2025-10-08 14:11:54 +0000

    Added paths to api endpoints

--- library_management_system/book/urls.py
+++ library_management_system/book/urls.py
@@ -10,4 +10,10 @@ urlpatterns = [
     path('manage_books/', book_views.manage_books, name='manage_books'),
     path('manage_users/', book_views.manage_users, name='manage_users'),
     path('borrow_records/', book_views.borrow_records, name='borrow_records'),
+    path('api/books/', book_views.ListBooks.as_view(), name='api_books'),
+    path('api/create/', book_views.CreateBooks.as_view(), name='create_books'),
+    path('api/retrieve/<int:pk>/', book_views.RetrieveBooks.as_view(), name='retrieve_books'),
+    path('api/delete/<int:pk>/', book_views.DeleteBooks.as_view(), name='delete_books'),
+    path('api/update/<int:pk>/', book_views.UpdateBooks.as_view(), name='update_books'),
+
 ]
